6966b6686142fe771a815888dfd72a4f3f6afbde
Gabriel Handford <gabrielh@gmail.com>
2020-03-04 18:35:52 -0800

    goreleaser

---
+++ cmd/updater/platform_linux.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/keys-pub/updater"
+	"github.com/pkg/errors"
+)
+
+func apply(options updater.UpdateOptions, sourcePath string, destinationPath string) error {
+	return errors.Errorf("unsupported platform")
+}
